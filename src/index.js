89d962218d0736cf9e6fbdc57d9210ba3161766e
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2022-12-27 18:06:27 +0800

    Second update with baseurl

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import App from './App';
 import { PersistGate } from 'redux-persist/integration/react';
 import {Provider} from 'react-redux'
 import { store, persistor } from './redux/store';
-import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
