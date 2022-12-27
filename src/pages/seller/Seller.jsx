bb3555a9411f81155f5a26276386bc2088fd13cc
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2022-12-27 12:55:45 +0800

    second commit

--- src/pages/seller/Seller.jsx
+++ src/pages/seller/Seller.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Typography } from '@mui/material';
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { publicRequest } from '../../utils.js/requestMethod'
 import BeatLoader from "react-spinners/BeatLoader";
 
